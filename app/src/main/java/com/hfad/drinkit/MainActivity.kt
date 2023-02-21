0c17ed6bba44ca5be8be7d1f204e3e1e5744c9ce
fareezatariq <fareezatariq@gmail.com>
2023-02-21 23:59:53 +0500

    added functions

--- app/src/main/java/com/hfad/drinkit/MainActivity.kt
+++ app/src/main/java/com/hfad/drinkit/MainActivity.kt
@@ -5,24 +5,50 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    private val Juice_State="JUICE_STATE"
-    private val Juice_Size="JUICE_SIZE"
-    private val Squeeze_count="SQUEEZE_COUNT"
+    private val JUICE_STATE="JUICE_STATE"
+    private val JUICE_SIZE="JUICE_SIZE"
+    private val SQUEEZE_COUNT="SQUEEZE_COUNT"
     private val SELECE="select"
     private val SQUEEZE="squeeze"
     private val DRINK="drink"
     private val RESTART="restart"
-    private var lemonState="select"
-    private var lemonSize=-1
+    private var juiceState="select"
+    private var juiceSize=-1
     private var squeezeCount=-1
-    private var lemonTree=LemonTree()
-    private var lemonImage: ImageView?= null
+    private var juiceTree=JuiceTree()
+    private var juiceImage: ImageView?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState!=null){
+            juiceState=savedInstanceState.getString("JUICE_STATE", "select")
+            juiceSize=savedInstanceState.getInt("JUICE_SIZE", -1)
+            squeezeCount=savedInstanceState.getInt(SQUEEZE_COUNT, -1)
+        }
+        juiceImage= findViewById(R.id.image_juice_state)
+        setViewElement()
+        juiceImage!!.setOnClickListener{
+            clickJuiceImage()
+        }
+        juiceImage!!.setOnLongClickListener {
+            showSnackbar()
+        }
+    }
+
+    private fun showSnackbar(): Boolean{
+        TODO("Not yet implemented")
+    }
+
+    private fun clickJuiceImage() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setViewElement() {
+        TODO("Not yet implemented")
     }
 }
-class LemonTree {
+class JuiceTree {
 fun pick(): Int{
     return (2..6).random()
 }
